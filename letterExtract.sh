feb6e7d4689aea70630e5c1200c68554517e2730
User <Willspri@gmail.com>
2025-01-07 16:48:32 +1300

    updated to 3rds

--- letterExtract.sh
+++ letterExtract.sh
@@ -5,7 +5,7 @@ extract_odds() {
 	input_string="$1"
 	output_string=""
 
-	for (( i=1; i<${#input_string}; i+=2 )); do
+	for (( i=2; i<${#input_string}; i+=3 )); do
 		output_string="${output_string}${input_string:i:1}"
 	done
 
